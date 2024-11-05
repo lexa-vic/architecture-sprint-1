526a51f30d80c51c7541ccb244062c2e0704eeca
al.kostikov <al.kostikov@corp.vk.com>
2024-11-05 13:07:24 +0300

    Init commit with first task

--- frontend/microfrontend/main-host-microfrontend/src/components/App.js
+++ frontend/microfrontend/main-host-microfrontend/src/components/App.js
@@ -3,18 +3,18 @@ import { Route, useHistory, Switch } from "react-router-dom";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-import PopupWithForm from "./PopupWithForm";
-import ImagePopup from "./ImagePopup";
-import api from "../utils/api";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import EditProfilePopup from "./EditProfilePopup";
-import EditAvatarPopup from "./EditAvatarPopup";
-import AddPlacePopup from "./AddPlacePopup";
-import Register from "./Register";
-import Login from "./Login";
-import InfoTooltip from "./InfoTooltip";
-import ProtectedRoute from "./ProtectedRoute";
-import * as auth from "../utils/auth.js";
+import PopupWithForm from "../../../../src/components/PopupWithForm.js";
+import ImagePopup from "../../../../src/components/ImagePopup.js";
+import api from "../../../main-profile-microfrontend/src/utils/api.js";
+import { CurrentUserContext } from "../../../main-profile-microfrontend/src/components/CurrentUserContext.js";
+import EditProfilePopup from "../../../main-profile-microfrontend/src/components/EditProfilePopup.js";
+import EditAvatarPopup from "../../../main-profile-microfrontend/src/components/EditAvatarPopup.js";
+import AddPlacePopup from "../../../../src/components/AddPlacePopup.js";
+import Register from "../../../auth-microfrontend/src/components/Register.js";
+import Login from "../../../auth-microfrontend/src/components/Login.js";
+import InfoTooltip from "../../../auth-microfrontend/src/components/InfoTooltip.js";
+import ProtectedRoute from "../../../../src/components/ProtectedRoute.js";
+import * as auth from "../../../auth-microfrontend/src/utils/auth.js";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
